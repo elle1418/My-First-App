b9f78a4e7354c801190a6e137a39e547fd9e069d
elle1418 <elcua0614@gmail.com>
2021-10-26 01:25:51 +0900

    create index page

--- backend/resources/views/hello/index.blade.php
+++ backend/resources/views/hello/index.blade.php
@@ -1,14 +1,14 @@
 <html>
   <head>
-    <title>Hello/Index</title>
+    <title>憎悪も恐怖も後悔も</title>
     <style>
       body {font-size:16pt; color:#999;}
-      h1 {font-size:50pt; text-align:right; color:#f6f6f6;
+      h1 {font-size:50pt; text-align:right; color:#f0f0f0;
           margin:-20px 0px -30px 0px; letter-spacing:-4pt;}
     </style>
   </head>
   <body>
-    <h1>Blade/Index</h1>
+    <h1>全て出しきれ<br>拳に乗せろ</h1>
     <p>&#064;forディレクティブの例</p>
       @foreach ($data as $item)
       @if ($loop->first)
